885fe368d78dab36366f4627d799a5c2aee387bb
Steven Chia <stevenchia56@gmail.com>
2022-02-06 19:34:23 +0800

    test run 1

--- main.tf
+++ main.tf
@@ -1,16 +1,11 @@
 provider "google" {
-  credentials = file("learn-terraform-337209-bb651da8f4c0.json")
-  project     = "learn-terraform-337209"
-  region      = "asia-southeast1"
+  project = "learn-terraform-337209"
+  region  = "asia-southeast1"
 }
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
-
-  metadata = {
-    ssh-keys = "poimgs:${file("~/.ssh/id_ed25519.pub")}"
-  }
 
   boot_disk {
     initialize_params {
